a6ea1c60593d69ca8156bf1ae2a9ce22c8eaefed
조익 <dream-ik89@naver.com>
2023-11-03 16:53:44 +0900

    temp: 임시저장(error)

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,8 +1,8 @@
 import {connectionMongoDb} from "@/app/util/client";
 
-export default function getList(){
-    // const productListTitle = "Products";
-    // const client = await connectionMongoDb();
-    // const db = client.db("forum");
-    // const data = await db.collection('post').find().toArray();//특정 컬렉션의 데이터를 array로 받기
+export default async function getList(){
+    const client = await connectionMongoDb();
+    const db = client.db("forum");
+
+    return await db.collection('post').find().toArray();//특정 컬렉션의 데이터를 array로 받기
 }
