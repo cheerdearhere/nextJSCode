83e1d4a76ff2bbac08434fe29b6cf4b9a83d17c8
조익 <dream-ik89@naver.com>
2023-11-03 13:40:15 +0900

    feat-page: list, detail 페이지

--- app/detail/[id]/page.js
+++ app/detail/[id]/page.js
@@ -1,10 +1,10 @@
 import {connectionMongoDb} from "@/app/util/client";
 import {ObjectId} from "mongodb";
 
-export default async function Detail(){
+export default async function Detail(props){
     let db = (await connectionMongoDb()).db('forum');
 
-    let result = await db.collection('post').findOne({_id : new ObjectId('')});
+    let result = await db.collection('post').findOne({_id : new ObjectId(props.params.id)});
     return (
         <div>
             <h4>상세페이지</h4>
