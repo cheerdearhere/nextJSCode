8046f02500e8c119cf927ef3bbee4571f4c1be53
조익 <dream-ik89@naver.com>
2023-11-08 17:43:17 +0900

    fix: 컴포넌트 수정으로인한 import 수정

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
-export default function Home() {
-    return <h1>Home</h1>
+import {apiLinks} from "@/app/components/serverSide/linkList";
+
+export default async function Home() {
+    let clock= await fetch(apiLinks.getDatTime,{}).then(r=>r.json());
+    return (
+        <div className="container">
+            <h1>Home</h1>
+            <h3>{clock}</h3>
+        </div>
+    )
 }
