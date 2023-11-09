13f83468e06b364e4cf1dce87e73a73112a7c1c8
조익 <dream-ik89@naver.com>
2023-11-09 17:58:12 +0900

    feat: form으로 내용입력 추가(validation 처리 필요)

--- pages/api/list.js
+++ pages/api/list.js
@@ -6,7 +6,7 @@ export default async function getList(resquest,response){
     const client = await connectionMongoDb();
     const db = client.db("forum");
     let list = await db.collection('post').find().toArray();//특정 컬렉션의 데이터를 array로 받기
-    if(list.length ===0) return response.status(HttpResponseStatus.NO_DATA.CODE).json(HttpResponseStatus.NO_DATA.MESSAGE);
+    if(list.length ===0) return response.status(HttpResponseStatus.NO_CONTENT.CODE).json(HttpResponseStatus.NO_CONTENT.MESSAGE);
 
-    return response.status(HttpResponseStatus.SUCCESS.CODE).json(list);
+    return response.status(HttpResponseStatus.OK.CODE).json(list);
 }
