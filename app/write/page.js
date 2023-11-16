0c03185e0658fd5c8ea8a34a96f1d83e1e1959b7
조익 <dream-ik89@naver.com>
2023-11-16 16:40:20 +0900

    info(feat): react 문법을 사용하려고 했으나 아직 next.js에서 csr은 await,async를 지원하지 않음 > 주석처리

--- app/write/page.js
+++ app/write/page.js
@@ -1,19 +1,60 @@
+// 'use client'
 import {apiLinks} from "@/app/components/serverSide/linkList";
+// import {useRouter} from "next/navigation";
 import {HTTP_METHODS} from "next/dist/server/web/http";
+// import {useState} from "react";
 
 export default async function WriteForm(){
+    // const [formData,setFormData] = useState({
+    //     title:"",
+    //     content:"",
+    // });
+    // const handleForm = e=>{
+    //     const {name,value} = e.target;
+    //     setFormData(prev=> ({ ...prev, [name]: value }));
+    // }
+    // const saveAndRedirect=(e)=>{
+    //     e.preventDefault();
+    //
+    //     let sendData = new FormData();
+    //     sendData.append("body",new Blob([JSON.stringify(sendData),{type:"application/json"}]));
+    //     const postOpt = {
+    //         type: "application/json",
+    //         headers: {
+    //             "Content-Type": "multipart/form-data",
+    //         },
+    //     }
+    //     const result = fetch(apiLinks.addPost,postOpt,sendData);
+    //     console.log(result);
+    //     const router = new useRouter();
+    //     router.push(result.toString());
+    //
+    // }
     return(
         <div className="p-20 container">
             <form action={apiLinks.addPost} method={HTTP_METHODS[3]}>
                 <div className="form-group">
                     <label htmlFor="title">제목</label>
-                    <input id="title" name="title" className="form-control" placeholder="여기에 입력" required={true}/>
+                    <input
+                        name="title"
+                        className="form-control"
+                        placeholder="여기에 입력"
+                        required={true}
+                        // onChange={e=>handleForm(e)}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="content">내용</label>
-                    <input id="content" name="content" className="form-control" placeholder="내용은 여기" required={true}/>
+                    <input
+                        name="content"
+                        className="form-control"
+                        placeholder="내용은 여기"
+                        required={true}
+                        // onChange={e=>handleForm(e)}
+                    />
                 </div>
                 <button type="submit">등록</button>
+                {/*<button onClick={e=>saveAndRedirect(e)}>등록</button>*/}
             </form>
         </div>
     );
